8a03397066087ff4c7d4438ad05d000a8bc5d9d6
DanielLChang <daniellawrencechang@gmail.com>
2016-12-30 00:46:33 -0800

    Finish phase 1. Wrote Note class and TONE/NOTENAMES constants.

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -4,9 +4,16 @@ import ReactDOM from 'react-dom';
 // import configureStore from './store/store';
 // import Root from './components/root';
 
+class Root extends React.Component {
+  render () {
+    return (
+      <div></div>
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   // const store = configureStore();
   const rootEl = document.getElementById('root');
-  // ReactDOM.render(<Root store={store} />, rootEl);
+  ReactDOM.render(<Root />, document.getElementById('root'));
 });
