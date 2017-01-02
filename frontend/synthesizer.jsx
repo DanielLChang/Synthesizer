80537fdac93c1589ad111f81accd330e984bbf9f
DanielLChang <daniellawrencechang@gmail.com>
2017-01-01 20:10:06 -0800

    Create redux store

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import configureStore from './store/store';
-// import Root from './components/root';
-
-class Root extends React.Component {
-  render () {
-    return (
-      <div></div>
-    );
-  }
-}
+import Note from './util/note';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const store = configureStore();
+  const store = configureStore();
   const rootEl = document.getElementById('root');
-  ReactDOM.render(<Root />, document.getElementById('root'));
+  ReactDOM.render(<Root store={store} />, rootEl);
 });
