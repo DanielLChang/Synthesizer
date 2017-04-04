f08a6c10f9e13737e29f4ead2120d157c4e253ee
DanielLChang <daniellawrencechang@gmail.com>
2017-04-04 14:57:41 -0700

    App and root components

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store';
 
+import configureStore from './store/store';
 import Note from './util/note';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<div>"Hello"</div>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 
   // For testing
   window.store = store;
