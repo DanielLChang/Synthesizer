82d3a9290b46c1ba906f521717be6812f57e147d
DanielLChang <daniellawrencechang@gmail.com>
2017-04-04 14:50:26 -0700

    Configure store

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 import Note from './util/note';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<div>"Hello"</div>, root);
+
+  // For testing
+  window.store = store;
 });
