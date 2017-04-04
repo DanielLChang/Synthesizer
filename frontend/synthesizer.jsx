0971709b24861a6fca927355b0bc3168a4a0a7b2
DanielLChang <daniellawrencechang@gmail.com>
2017-04-04 14:16:58 -0700

    Note class

--- frontend/synthesizer.jsx
+++ frontend/synthesizer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Note from './util/note';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<div>"Hello"</div>, root);
